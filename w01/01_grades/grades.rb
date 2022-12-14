14a7a4f2ba44df8844d05e5fa74997650b572a92
Jonathan <jonitoadriano@gmail.com>
2022-12-13 22:24:31 -0600

    test

--- w01/01_grades/grades.rb
+++ w01/01_grades/grades.rb
@@ -1,6 +1,27 @@
 def final_grade(array)
-
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
-
+  if array == []
+    return "I"
+  end
+  i = 0
+  sum = 0
+  average = 0
+  while i < array.length
+    sum = sum + array[i].to_f
+    i += 1
+  end 
+  average = sum / array.length.round
+  average = average.round
+  puts average
+  # Associating code to grade
+  if average >= 90
+    return "A"
+  elsif average >= 80 && average <= 89
+    return "B"
+  elsif average >= 70 && average <= 79
+    return "C"
+  elsif average >= 60 && average <= 69
+    return "D"
+  elsif average < 60
+    return "F"    
+    end    
 end
